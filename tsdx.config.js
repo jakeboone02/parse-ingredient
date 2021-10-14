ad9c27cd21ecba06155e8c6b03d03590db3baf4f
Jake Boone <jakeboone02@gmail.com>
2021-10-14 12:05:37 -0700

    Update dependencies

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,8 +1,18 @@
+const replace = require('@rollup/plugin-replace');
+
 module.exports = {
-  rollup(config, _options) {
+  rollup(config, options) {
     config.output.name = 'parseIngredient';
     config.output.exports = 'default';
     config.output.globals = { 'numeric-quantity': 'numericQuantity' };
+    config.plugins = config.plugins.map(p =>
+      p.name === 'replace'
+        ? replace({
+            'process.env.NODE_ENV': JSON.stringify(options.env),
+            preventAssignment: true,
+          })
+        : p
+    );
     return config;
   },
 };
