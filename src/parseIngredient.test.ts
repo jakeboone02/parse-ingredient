aaabdf3dc1655c47d6147776d9e7f9396bd941a9
Jake Boone <jakeboone02@gmail.com>
2023-08-07 08:28:50 -0700

    Remove Jest types

--- src/parseIngredient.test.ts
+++ src/parseIngredient.test.ts
@@ -1,8 +1,9 @@
+import { expect, test } from 'bun:test';
 import { parseIngredient } from './parseIngredient';
 import { parseIngredientTests } from './parseIngredientTests';
 
 for (const [testName, [input, expected, options]] of Object.entries(parseIngredientTests)) {
-  it(testName, () => {
+  test(testName, () => {
     expect(parseIngredient(input, options)).toEqual(expected);
   });
 }
