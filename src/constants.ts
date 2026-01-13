aa62733ef11f555de9274792a5d75b2394cba29b
AfoxDesignz <45944543+AfoxDesignz@users.noreply.github.com>
2026-01-13 22:30:12 +0100

    fix: regex for UOMs also catching numerical chars

--- src/constants.ts
+++ src/constants.ts
@@ -33,7 +33,7 @@ export const rangeSeparatorRegEx: RegExp = new RegExp(`^${rangeSeparatorRegExSou
 /**
  * Regex to capture the first word of a description, to see if it's a unit of measure.
  */
-export const firstWordRegEx: RegExp = /^(fl(?:uid)?(?:\s+|-)(?:oz|ounces?)|\p{L}+[-.]?)(.+)?/u;
+export const firstWordRegEx: RegExp = /^(fl(?:uid)?(?:\s+|-)(?:oz|ounces?)|[\p{L}\p{N}_]+[-.]?)(.+)?/u;
 
 const numericRegexAnywhere = numericRegex.source.replace(/^\^/, '').replace(/\$$/, '');
 
